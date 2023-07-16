180b68f061423a235829703256347b63cfd9f9b9
Mosetii <zachy.techsasa@gmail.com>
2023-07-16 13:54:32 +0300

    API Update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let unirest = require('unirest');
-let req = unirest('POST', 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest')
-.headers({
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer bsevCfowDFzPJ3st1zndZTOSojHU'
-})
-.send(JSON.stringify({
+let headers = new Headers();
+headers.append("Content-Type", "application/json");
+headers.append("Authorization", "Bearer bsevCfowDFzPJ3st1zndZTOSojHU");
+
+fetch("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest", {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
     "BusinessShortCode": 174379,
     "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjMwNzE2MTM0NTIx",
     "Timestamp": "20230716134521",
@@ -16,8 +17,8 @@ let req = unirest('POST', 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/proc
     "CallBackURL": "https://mydomain.com/path",
     "AccountReference": "Elysium",
     "TransactionDesc": "Payment of X" 
-  }))
-.end(res => {
-    if (res.error) throw new Error(res.error);
-    console.log(res.raw_body);
-});
+  })
+})
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
